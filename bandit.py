225effc3230e80e92e9e68d5c5d731f2e5b6a225
Dimitri Rusin <dimitri@habimm.com>
2024-03-23 19:18:29 +0100

    Figure out optimal reward.

--- bandit.py
+++ bandit.py
@@ -1,79 +1,86 @@
-import gymnasium
+import copy
 import inspectify
 import numpy
 import plotly.graph_objects
 import plotly.subplots
 
-class MultiArmedBandit(gymnasium.Env):
-  def __init__(self, num_levers):
-    super(MultiArmedBandit, self).__init__()
-    self.action_space = gymnasium.spaces.Discrete(num_levers)
-    self.observation_space = gymnasium.spaces.Discrete(1)
-    self.num_optimal_selected = None
-    self.reward_means = None
-    self.optimal_action = None
+
+
+class MultiArmedBandit:
+  def __init__(self, levers):
+    self.levers = levers
+    self.action_space = list(range(levers))
+    self.best_lever = None
+    self.lever_rewards = None
+    self.best_chosen = 0
 
   def reset(self):
-    self.num_optimal_selected = 0
-    self.reward_means = numpy.random.normal(0.0, 1.0, self.action_space.n)
-    self.optimal_action = numpy.argmax(self.reward_means)
-    assert self.action_space.contains(self.optimal_action)
+    self.lever_rewards = numpy.random.normal(0.0, 1.0, self.levers)
+    self.best_lever = numpy.argmax(self.lever_rewards)
+    self.best_chosen = 0
+    return 0
 
-    return 0, {}  # Dummy state
+  def play(self, action):
+    assert action in self.action_space
 
-  def step(self, action):
-    assert self.action_space.contains(action)
+    if action == self.best_lever:
+      self.best_chosen += 1
 
-    if self.optimal_action == action:
-      self.num_optimal_selected += 1
+    reward = numpy.random.normal(self.lever_rewards[action], 1.0)
 
-    # Reward is drawn from a normal distribution centered at the mean of the chosen arm
-    reward = numpy.random.normal(self.reward_means[action], 1.0)
+    return reward
 
-    return 0, reward, True, False, {}
+  def copy(self):
+    return copy.deepcopy(self)
 
-def bandit_random(env, average_reward_per_action):
-  action_list = list(range(env.action_space.n))
-  action = numpy.random.choice(action_list)
+
+def bandit_random(env, sample_average):
+  action = numpy.random.choice(env.action_space)
   return action
 
-def epsilon_greedy(env, average_reward_per_action, epsilon=0):
+def epsilon_greedy(env, sample_average, epsilon=0):
   if numpy.random.random() < epsilon:
     # Exploration: randomly choose any action
-    return bandit_random(env, average_reward_per_action)
+    return bandit_random(env, sample_average)
   else:
     # Exploitation: choose the best action based on average rewards
-    max_average = max(average_reward_per_action.values())
-    best_actions = [action for action, avg_reward in average_reward_per_action.items() if avg_reward == max_average]
+    max_average = max(sample_average.values())
+    best_actions = [action for action, avg_reward in sample_average.items() if avg_reward == max_average]
     return numpy.random.choice(best_actions)
 
 def experiment(num_runs, num_timesteps, num_levers, seed, algorithm):
 
   numpy.random.seed(seed)
 
-  env = MultiArmedBandit(num_levers = num_levers)
+  env = MultiArmedBandit(levers = num_levers)
   rewards = numpy.zeros(num_timesteps)
-  num_optimal_selected = numpy.zeros(num_timesteps)
+  best_chosen = numpy.zeros(num_timesteps)
 
+  optimal_rewards = numpy.zeros(num_runs)
   for run in range(num_runs):
-    average_reward_per_action = {action:0 for action in range(env.action_space.n)}
-    action_visit_count = {action:0 for action in range(env.action_space.n)}
+    sample_count = {action:0 for action in env.action_space}
+    sample_average = {action:0 for action in env.action_space}
     state = env.reset()
+
+    optimal_reward = env.lever_rewards[env.best_lever]
+    optimal_rewards[run] = optimal_reward
+
     for timestep in range(1, num_timesteps + 1):
 
-      action = algorithm(env, average_reward_per_action)
-      _, reward, _, _, _ = env.step(action)
+      action = algorithm(env, sample_average)
+      reward = env.play(action)
 
-      action_visit_count[action] += 1
-      average_reward_per_action[action] += (1 / action_visit_count[action]) * (reward - average_reward_per_action[action])
+      sample_count[action] += 1
+      sample_average[action] += (1 / sample_count[action]) * (reward - sample_average[action])
 
       rewards[timestep - 1] += reward
-      num_optimal_selected[timestep - 1] += env.num_optimal_selected / timestep
+      best_chosen[timestep - 1] += env.best_chosen / timestep
 
   average_rewards = rewards / num_runs
-  average_num_optimal_selected = (num_optimal_selected / num_runs) * 100
+  average_num_optimal_selected = (best_chosen / num_runs) * 100
+  average_optimal_reward = optimal_rewards.mean()
 
-  return average_rewards, average_num_optimal_selected
+  return average_rewards, average_num_optimal_selected, average_optimal_reward
 
 if __name__ == '__main__':
   num_timesteps = 1_000
@@ -81,7 +88,7 @@ if __name__ == '__main__':
   num_levers = 10
   seed = 41
 
-  random_average_rewards, random_average_num_optimal_selected = experiment(
+  random_average_rewards, random_average_num_optimal_selected, average_optimal_reward = experiment(
     num_runs = num_runs,
     num_timesteps = num_timesteps,
     num_levers = num_levers,
@@ -89,28 +96,28 @@ if __name__ == '__main__':
     algorithm = bandit_random,
   )
 
-  greedy_average_rewards, greedy_average_num_optimal_selected = experiment(
+  greedy_average_rewards, greedy_average_num_optimal_selected, _ = experiment(
     num_runs = num_runs,
     num_timesteps = num_timesteps,
     num_levers = num_levers,
     seed = seed,
-    algorithm = lambda env, average_reward_per_action: epsilon_greedy(env, average_reward_per_action, epsilon = 0),
+    algorithm = lambda env, sample_average: epsilon_greedy(env, sample_average, epsilon = 0),
   )
 
-  greedy_epsilon_0_01_average_rewards, greedy_epsilon_0_01_average_num_optimal_selected = experiment(
+  greedy_epsilon_0_01_average_rewards, greedy_epsilon_0_01_average_num_optimal_selected, _ = experiment(
     num_runs = num_runs,
     num_timesteps = num_timesteps,
     num_levers = num_levers,
     seed = seed,
-    algorithm = lambda env, average_reward_per_action: epsilon_greedy(env, average_reward_per_action, epsilon = 0.01),
+    algorithm = lambda env, sample_average: epsilon_greedy(env, sample_average, epsilon = 0.01),
   )
 
-  greedy_epsilon_0_1_average_rewards, greedy_epsilon_0_1_average_num_optimal_selected = experiment(
+  greedy_epsilon_0_1_average_rewards, greedy_epsilon_0_1_average_num_optimal_selected, _ = experiment(
     num_runs = num_runs,
     num_timesteps = num_timesteps,
     num_levers = num_levers,
     seed = seed,
-    algorithm = lambda env, average_reward_per_action: epsilon_greedy(env, average_reward_per_action, epsilon = 0.1),
+    algorithm = lambda env, sample_average: epsilon_greedy(env, sample_average, epsilon = 0.1),
   )
 
   # Create a subplot figure with 2 rows and 1 column
